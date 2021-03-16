1a22c8a2922bbc9d87927e80bbf122e6b4f83791
Abilio Azevedo <abilio10@gmail.com>
2021-03-15 22:27:37 -0300

    refactor: components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import "../styles/sidebar.scss";
+import { GenreResponseProps, MovieProps } from "../App";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
 }
+
+export const SideBar: React.FC<SideBarProps> = ({
+  genres,
+  selectedGenreId,
+  handleClickButton,
+}) => (
+  <nav className="sidebar">
+    <span>
+      Watch<p>Me</p>
+    </span>
+
+    <div className="buttons-container">
+      {genres.map((genre) => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+);
